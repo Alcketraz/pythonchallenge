43c56aa19ecdf657ec4d19452ced7866c970fd0f
Alcketraz <56187190+Alcketraz@users.noreply.github.com>
2020-11-23 18:26:55 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -3,3 +3,10 @@
 a= "I want to be a python programmer"
 #2nd value taken considering indexing from 0
 print(a[1:])
+
+
+OUTPUT:
+  C:\python37\python.exe C:/Users/Vedant/PycharmProjects/pythonInternship/day1.py
+ want to be a python programmer
+
+Process finished with exit code 0
