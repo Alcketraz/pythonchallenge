8819093951e29b8ece9d7dca8644c5b0f2df5a63
Alcketraz <56187190+Alcketraz@users.noreply.github.com>
2020-11-23 16:59:32 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,28 @@
+# Hours = "thirty"
+# print(Hours)
+#
+# days= "Thirty days"
+# print(days[0])
+#
+# Challenge = "I will win"
+# print(len(Challenge))
+#
+# print(Challenge.lower())
+#
+# a = "30 Days"
+# b = "30 hours"
+# c = a + b
+# print(c)
+#
+#
+# a = "30 Days"
+# b = "30 hours"
+# c = a + " " + b
+# print(c)
+
+# test= "Thirty days and thirty hours"
+# print(test.casefold())
+
+a= "I want to be a python programmer"
+#2nd value taken considering indexing from 0
+print(a[1:])
