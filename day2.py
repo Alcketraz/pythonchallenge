c33f36ad81714fe35c9035d807f66c5f61d5b833
Alcketraz <56187190+Alcketraz@users.noreply.github.com>
2020-11-24 13:55:33 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,30 @@
+#Q1)
+a = b = c = 20
+a = a/10
+b = b*50
+c = c+60
+
+print("a= ", a)
+print("b= ", b)
+print("c= ", c)
+
+#Q2)
+
+str = "SHADY"
+str1 = str.replace("A", "G")
+
+print(str1)
+
+#Q3)
+
+a = 2
+b = 4.5462
+print("Before converting")
+print(a)
+print(b)
+
+a = float(a)
+b = int(b)
+print("After conversion")
+print(a)
+print(b)
